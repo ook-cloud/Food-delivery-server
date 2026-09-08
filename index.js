5370a5566394ba9ea445342f112f0fc36efbb166
ook-cloud <ooktb57@gmail.com>
2026-09-08 10:18:26 +0800

    tday

--- index.js
+++ index.js
@@ -12,42 +12,13 @@ connectDB();
 
 app.get("/api/health", (request, response) => {
   response.json({ message: `API HEALTHY RUNNING ON ${PORT}` });
-}); //read
-
-app.post("/sign-up", async (request, response) => {
-  try {
-    const { email, password } = request.body;
-    const user = await User.create({ email, password });
-    response.status(201).json({ message: "user created", user: user });
-  } catch (err) {
-    response.status(500).json({ message: "internal Server Error", error: err });
-  }
 });
 
-// app.post("/food-category", async (request, response) => {});
-app.post("/login", async (request, response) => {
-  try {
-    throw new Error("hello");
+app.post("/login",);
 
-    const { email, password } = request.body;
-    console.log(email, password);
-    const user = await User.findOne({ email: email });
-    if (!user) {
-      response.status(404).json({ message: "user not found" });
-    }
-    response.status(200).json({ message: "user found" });
-  } catch (err) {
-    response.status(500).json({ message: "internal Server Error", error: err });
-  }
-});
+app.post("/sign-up",);
 
-app.delete("/api/health", (request, response) => {
-  response.json({ message: `YOU ARE CALLING DELETE ${PORT}` });
-}); // delete
-
-app.put("/api/health", (request, response) => {
-  response.json({ message: `YOU ARE CALLING UPDATE ${PORT}` });
-}); // update
+app.post("/food/category", async (request, response) => 
 
 app.listen(PORT, () => {
   console.log("server is running, on port ${PORT}");
